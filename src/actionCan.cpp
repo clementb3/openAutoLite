f75e209344fa64980db46483c5b5a897efc1e8e1
clement boulay <boulayclement2003@gmail.com>
2026-04-14 19:09:42 +0200

    fix + and - button

--- src/actionCan.cpp
+++ src/actionCan.cpp
@@ -5,7 +5,8 @@ namespace openauto {
 	void actionCan::onMessage(const service::CanMessage &msg) {
 		switch (msg.id) {
 			case 0x1CA:
-				std::cout << "[Can receive message] id : " << msg.id << "content" << msg.data[0] << msg.data[1] <<
+				std::cout << "[Can receive message] id [" << msg.data.size() << "] " << msg.id << " : " << msg.data[0]
+						<< ":" << msg.data[1] <<
 						std::endl;
 				if (msg.data.size() == 4 && msg.data[0] == 0x03) {
 					switch (msg.data[1]) {
